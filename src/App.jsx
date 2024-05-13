f0615230f2eb940ee75112709165ff54719ec0d3
Mintu Sikder <mintusikder15@gmail.com>
2024-05-13 23:39:26 +0600

    added user information

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import "./App.css";
 
 function App() {
+  const handelAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+  };
   return (
     <>
       <h1>Simple CRUD</h1>
+      <form onSubmit={handelAddUser}>
+        <input type="text" name="name" id="name" /> <br />
+        <input type="text" name="email" id="email" /> <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   );
 }
